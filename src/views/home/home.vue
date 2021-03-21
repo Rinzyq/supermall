b286f052a51184d34946dc1e21ea6a6787f5613c
Rin <87538369@qq.com>
2021-03-21 18:39:14 +0800

    feature-view

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,6 +3,7 @@
         <nav-bar class="home-nav"><template v-slot:center>购物街</template></nav-bar>
         <home-swiper :banners="banners"></home-swiper>
         <recommend-view :recommends="recommends"></recommend-view>
+        <feature-view></feature-view>
     </div>
 </template>
 
@@ -10,6 +11,7 @@
 import NavBar from "components/common/navbar/navbar"
 import HomeSwiper from "./childComp/homeswiper"
 import RecommendView from "./childComp/recommendview"
+import FeatureView from "./childComp/featureview"
 import {getHomeMultidata} from "network/home"
 
 export default {
@@ -17,7 +19,8 @@ export default {
     components:{
         NavBar,
         HomeSwiper,
-        RecommendView
+        RecommendView,
+        FeatureView
     },
     data(){
         return {
@@ -30,7 +33,6 @@ export default {
         getHomeMultidata().then(res=>{
             this.banners=res.data.banner.list;
             this.recommends=res.data.recommend.list;
-            console.log(this.recommends);
         })
     },
 }
@@ -40,5 +42,14 @@ export default {
     .home-nav{
         background-color: var(--color-tint);
         color: #fff;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 10;
+    }
+    #home{
+        padding-top: 44px;
+        height: 1000px;
     }
 </style>
