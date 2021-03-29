dcf74b43758136fd8d7332449ac2de298e12a37d
Rin <87538369@qq.com>
2021-03-29 10:55:07 +0800

    解决移动端300ms延迟

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import router from './router'
 import store from './store'
 //引入toast
 import toast from "common/toast"
+//引入fastclick
+import FastClick from "fastclick"
 
 Vue.config.productionTip = false
 //安装toast插件
 Vue.use(toast);
+//解决移动端300ms延迟
+FastClick.attach(document.body);
 
 new Vue({
   router,
